a8f168f9ca2b9f8f5943d56220704c1f397f322f
newProject <tthrishagb@gmail.com>
2026-08-03 15:24:24 +0530

    Add 66 Plus One solution

---
+++ Arrays/066-Plus-One.java
@@ -0,0 +1,30 @@
+/*
+Problem: 66. Plus One
+Pattern: Arrays
+Difficulty: Easy
+
+Approach:
+- Traverse the array from the last digit.
+- If the current digit is less than 9, increment it and return.
+- Otherwise, set it to 0 and continue carrying.
+- If all digits become 0, create a new array with a leading 1.
+
+Time Complexity: O(n)
+Space Complexity: O(n) (only when a new array is created)
+*/
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
