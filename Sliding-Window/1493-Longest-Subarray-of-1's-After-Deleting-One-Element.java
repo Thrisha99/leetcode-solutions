effcd182bbbc490f2ea312b35ccf889a39e8daa4
Thrisha G B <thrishareddygb@gmail.com>
2026-08-26 14:51:23 +0530

    Update 1493-Longest-Subarray-of-1's-After-Deleting-One-Element.java

--- Sliding-Window/1493-Longest-Subarray-of-1's-After-Deleting-One-Element.java
+++ Sliding-Window/1493-Longest-Subarray-of-1's-After-Deleting-One-Element.java
@@ -48,27 +48,19 @@
 class Solution {
     public int longestSubarray(int[] nums) {
         int l=0;
-        int max=0;
-        int length=0;
         int z=0;
-        int k=1;
-        for(int r=0;r<nums.length;r++){
-            if(nums[r]==0){
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
                 z++;
             }
-            while(z>k){
+           while(z>1){
                 if(nums[l]==0){
                     z--;
                 }
                 l++;
             }
-            if(z<=k){
-                length=r-l+1;
-                max=Math.max(max,length-1);
-            }
-        }
-        if(max==0){
-            return 0;
+            max=Math.max(max,i-l);
         }
         return max;
     }
